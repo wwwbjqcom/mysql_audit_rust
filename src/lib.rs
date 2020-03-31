81d5a7c59e714ac243adaa63bac8e00bd915171d
manito <wwwbjqcom@127.com>
2020-03-31 11:17:55 +0800

    1

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,7 @@ pub fn op_run() -> std::result::Result<(), Box<dyn Error>> {
                 if !check_ack_syn(&my_packet){                                                      // 根据flag头再次判断是否为ack/syc包
                     continue 'inner;
                 }
-                println!("{}, {}, {}", my_packet.packet_flag, my_packet.len, my_packet.data_cur.tell());
+                println!("{}, {}, {}", my_packet.packet_flag, my_packet.len, my_packet.data_cur.tell().unwrap());
                 my_packet.get_mysql_protocol_header()?;                                             // 获取mysql协议header部分
                 //println!("{:?}",&my_packet.protocol_header);
                 if my_packet.check_port(&conf){                                                     // 判断数据流向端口是否为给定的端口
