2fdb3b0b833b43ad9e1b08241b08c9a7ffe6c3a7
manito <wwwbjqcom@127.com>
2020-03-31 12:00:18 +0800

    1

--- src/lib.rs
+++ src/lib.rs
@@ -109,8 +109,8 @@ pub fn op_run() -> std::result::Result<(), Box<dyn Error>> {
                     sfile.write(&packet);
                     let session_key = my_packet.set_stream_type(&conf)?;
                     my_packet.get_mysql_protocol_header()?;                                             // 获取mysql协议header部分
-                    println!("{:?}, {:?}, {:?}", my_packet.session_host_info, my_packet.s_type, my_packet.protocol_header);
-                    //my_packet.op_session_info(&session_key, &mut all_session_info)?;
+                    //println!("{:?}, {:?}, {:?}", my_packet.session_host_info, my_packet.s_type, my_packet.protocol_header);
+                    my_packet.op_session_info(&session_key, &mut all_session_info)?;
                 }
             }
         }
